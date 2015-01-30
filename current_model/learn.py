f267a5bf8cd1ddc7aa8687837a79ea7dbd3efa5f
Erin Grant <e.grant41@gmail.com>
2015-01-30 17:07:36 -0500

    fix bug with alpha in numerator

--- current_model/learn.py
+++ current_model/learn.py
@@ -434,8 +434,8 @@ class Learner:
 
         self._learned_lexicon.set_unseen(word,
             basic_unseen_tuples,
-            1./basic_denom,
-            1./sup_denom)
+            self.alpha_basic/basic_denom,
+            self.alpha_sup/sup_denom)
 
     def association(self, word, feature):
         """
